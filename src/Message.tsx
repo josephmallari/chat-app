24bbdf3355367757e0ea4f495aeeae0b1e86fe8e
josephmallari <joseph.mallari48@gmail.com>
2021-01-23 19:55:30 +0100

    set up reply feature

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,12 +1,30 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useState, useRef } from 'react'
 
 interface Props {
-  text: string;
+  text: string
 }
 
+// on click blur all elements
 export default function Message({text}: Props): ReactElement {
+  const [replyState, setReplyState] = useState<boolean>(false);
+  const [replyText, setReplyText] = useState<string>('');
+  const replyRef = useRef<HTMLDivElement>(null);
+
+  function reply() {
+    setReplyState(!replyState);
+    document.querySelectorAll('.message').forEach(message => message.classList.add('blur'));
+
+    if (replyRef && replyRef.current) {
+      replyRef.current.classList.remove('blur');
+      setReplyText(replyRef.current.innerHTML);
+    }
+  }
+
+  const replyClass = '';
+  // const replyClass = replyState ? ' blur' : '';
+
   return (
-    <div className="message">
+    <div ref={replyRef} onClick={reply} className={`message${replyClass}`}>
      {text} 
     </div>
   )
